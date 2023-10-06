0fc46a2aab65e363b7e86077a71bea32a5a8b03a
Emmanuel <adeloyee0220@students.bowiestate.edu>
2023-10-06 06:41:14 -0700

    lab 2 files

--- Lab2partb.py
+++ Lab2partb.py
@@ -8,4 +8,7 @@ mult=number1*number2*number3*number4
 subtract=number4-number3-number2-number1
 division=(number4/number3)
 
-print(add,mult,subtract,division)
+print(add)
+print(mult)
+print(subtract)
+print(division)
